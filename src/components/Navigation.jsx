cd92ce333fa733ee09a195dae879914b65910e6c
Kashsan <kashsan_jp@yahoo.co.jp>
2025-08-14 10:33:06 +0900

    2025.8.14.10:02

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,12 +9,13 @@ export default async function Navigation() {
     // console.log(authUser);
 
   return (
-    <nav>
-        <NavLink label="Home" href="/" />
-        
+    <nav className='lg:h-5 sm:h-2'>
+        <div className='lg:mr-150 sm:mr-0'>
+            <NavLink label="Home" href="/" />
+        </div>
             { authUser ? 
                 (
-                    <div className='flex items-center lg:mx-[2px] sm:mx-1'>
+                    <div className='flex items-center sm:text-sm lg:text-lg sm:gap-0.1'>
                         <NavLink label="New Post" href="/posts/create" />
                         <NavLink label="Dashboard" href="/dashboard" />
                         <form action={logout}>
