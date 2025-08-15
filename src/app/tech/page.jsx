dc272ba65b30c3d4f260255de9c38d9026bccaff
Kashsan <kashsan_jp@yahoo.co.jp>
2025-08-15 22:26:37 +0900

    2025.8.15.17:03

--- src/app/tech/page.jsx
+++ src/app/tech/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Tech() {
+
   return (
     <div className='body'>
       <div className='title'>このWebアプリの仕様</div>
@@ -11,15 +12,8 @@ export default function Tech() {
       </div>
       <p className='text-center text-green-700 sm:text-sm lg:text-xl'>自己紹介</p>
       <div className='p-2 border border-solid rounded m-auto lg:w-lg sm:w-sm border-green-700'>
-        わたしが真剣にプログラミングに取り組み始めたのは2025年の2月ごろからです。 <br></br>
-        そして初めてReactアプリ(バックエンドはBaaS)をデプロイできたのは同年3月中頃です。
-        それからLaravelを真剣に勉強しはじめて、同年6月にはVPSにLinuxサーバーを構築し、 Laravel inertia Vue で作ったSNSアプリをデプロイしました。 現在は Laravel Livewire を使って民泊紹介サイトを製作中です。
-
-        この半年間、真剣にプログラミングに取り組んできました。 日々エラーとの戦いを何とか克服し前進してきました。
-        私が今まで個人開発を止めずに続けられたのは、「良いサービスを作りたい」 という目的意識を持っていたからです。
-        これからもどんどん新しい技術を吸収して皆様に喜んでもらえるような サービスを作っていくのが私の夢です。
-
-        Webエンジニアになることが目的ではなく、気づいたらWebエンジニアになっていたと 思えるような自分でありたいと思っています。
+        1997年HTMLでWebコーディングを始め、 その後 K-Ash.com を立ち上げる。 社会貢献のため 障害者のために MHPコンサートをスタート、 2017年にSwiftで開発を始める。 2020年からReactNative,Flutterの モバイルアプリ開発を始めると同時に React、Vue、Laravel、Nodejsにも 興味を持ち開発を始める。
+2025年3月に介護福祉関係の会社を退職し、 現在、個人開発でWebサービス作りに励んでいます。
       </div>
     </div>
   )
