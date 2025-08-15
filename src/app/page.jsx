4d633acb1effcc52a7d2bf2f9fa68d9e2d7d81a1
Kashsan <kashsan_jp@yahoo.co.jp>
2025-08-16 01:19:08 +0900

    2025.8.15.17:09

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,7 @@ export default async function Home() {
         <h1 className="title"> <span className="text-red-500">K</span>ashsan 技術ブログ</h1>
         <p className="text-green-700 mb-10">
         シニアエンジニアKashsanの<br></br>
-        開発技術の向上を記録しています。
+        開発技術を記録しています。
         </p>
           <Link href="tech">
           <div className="text-red-500 font-bold font-serif font-lg">Tech Memo</div>
