9b01349ea8a734d9383461c0efe5a3d95a61133e
Kashsan <kashsan_jp@yahoo.co.jp>
2025-08-16 20:14:00 +0900

    2025.8.16.７

---
+++ src/app/link/links.js
@@ -0,0 +1,8 @@
+export const links = [
+  { id:1, date:"2007", siteName:"K-Ash.com", url: "https://www.k-ash.com", comment: "HTML CSS JavaScript", },
+  { id:2, date:"2008", siteName:"MHPコンサート", url: "https://www.k-ash.com/mhp", comment: "HTML CSS JavaScript jQueryMobile", },
+  { id:3, date:"2010", siteName:"和菓子の福屋", url: "https://www.k-ash.com/fukuya", comment: "HTML CSS JavaScript jQueryMobile", },
+  { id:4, date:"2025", siteName:"トラベル(Demo)", url: "https://www.k-ash.com/travel_website", comment: "HTML CSS JavaScript jQuery", },
+  { id:5, date:"2025", siteName:"SNS(技術者用)", url: "https://k-ashtestsite.ddns.net", comment: "Vue Laravel Linux Server", },
+
+]
--- src/app/link/page.jsx
+++ src/app/link/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { links } from './links'
+
+const jsonLinks = links
 
 export default function Link() {
   return (
+    <>
      <div className='body'>
       <div className='title'><span className='text-red-500'>K</span>ashsan 製作サイト</div>
+      <p className='text-center text-green-700'>サイト名をクリックするとリンク先に飛びます</p>
       <table>
         <thead>
             <tr>
@@ -13,34 +18,18 @@ export default function Link() {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td className='w-1/6'>2002年</td>
-                <td className='w-1/6'><a href='https://www.k-ash.com'>K-Ash.com</a></td>
-                <td className='w-3/6'>HTML CSS JavaScript</td>
-            </tr>
-            <tr>
-                <td className='w-1/6'>2008年</td>
-                <td className='w-1/6'><a href='https://www.k-ash.com/mhp'>MHPコンサート</a></td>
-                <td className='w-3/6'>HTML CSS JavaScript jQuery</td>
-            </tr>
-            <tr>
-                <td className='w-1/6'>2010年</td>
-                <td className='w-1/6'><a href='https://www.k-ash.com/fukuya'>和菓子の福屋</a></td>
-                <td className='w-3/6'>HTML CSS JavaScript jQuery</td>
-            </tr>
-            <tr>
-                <td className='w-1/6'>2025年</td>
-                <td className='w-1/6'><a href='https://www.k-ash.com/travel_website'>トラベル(Demo)</a></td>
-                <td className='w-3/6'>HTML CSS JavaScript jQuery</td>
-            </tr>
-            <tr>
-                <td className='w-1/6'>2025年</td>
-                <td className='w-1/6'><a href='https://k-ashtestsite.ddns.net'>SNSアプリ</a></td>
-                <td className='w-3/6'>Vue Laravel Linux Server</td>
-            </tr>
+            {jsonLinks.map((link) => (
+                <tr key={link.id}>
+                    <td>{link.date}</td>
+                    <td><a href={link.url}>{link.siteName}</a></td>
+                    <td>{link.comment}</td>
+                </tr>
+            ))
+            }
         </tbody>
         
       </table>
     </div>
+    </>
   )
 }
