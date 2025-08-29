4fe46421bd09a43045c96215ada8b48916514b00
Kashsan <kashsan_jp@yahoo.co.jp>
2025-08-29 16:23:14 +0900

    2025.8.29.1

--- src/app/link/links.js
+++ src/app/link/links.js
@@ -5,7 +5,7 @@ export const links = [
   { id:4, date:"2025", siteName:"和菓子のブログ", url: "https://fukuya-blog.vercel.app", comment: "Nextjs MongoDB", },
   { id:5, date:"2025", siteName:"トラベルサイト(Demo)", url: "https://www.k-ash.com/travel_website", comment: "HTML CSS JavaScript jQuery", },
   { id:6, date:"2025", siteName:"SNS(技術者用)", url: "https://kashsan-testsite.top", comment: "Vue Laravel Linux Server iTerm2(SSH接続)", },
-  { id:7, date:"2025", siteName:"猫ジェネ", url: "https://k-ash-dotcom-test.s3.ap-northeast-1.amazonaws.com/index.html", comment: "HTML Javascript AWS S3", },
+  { id:7, date:"2023", siteName:"猫ジェネ", url: "https://k-ash-dotcom-test.s3.ap-northeast-1.amazonaws.com/index.html", comment: "HTML Javascript AWS S3", },
   { id:8, date:"2025", siteName:"当サイト", url: "https://next-blog-app-8uuo.vercel.app", comment: "Nextjs MongoDB", },
 
 ]
