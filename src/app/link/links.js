cdbfa6576a3974f5e2a06b44e6b544c7440a7aac
Kashsan <kashsan_jp@yahoo.co.jp>
2026-01-26 04:16:18 +0900

    2026.1.26_5

--- src/app/link/links.js
+++ src/app/link/links.js
@@ -3,10 +3,11 @@ export const links = [
   { id:2, date:"2008", siteName:"MHPコンサート", url: "https://www.k-ash.com/mhp", comment: "HTML CSS JavaScript jQueryMobile", },
   { id:3, date:"2010", siteName:"和菓子の福屋", url: "https://www.k-ash.com/fukuya", comment: "HTML CSS JavaScript jQueryMobile", },
   { id:4, date:"2025", siteName:"和菓子のブログ", url: "https://fukuya-blog.vercel.app", comment: "Nextjs MongoDB", },
-  { id:5, date:"2025", siteName:"トラベルサイト(Demo)", url: "https://www.k-ash.com/travel_website", comment: "HTML CSS JavaScript jQuery", },
-  { id:6, date:"2025", siteName:"SNS(技術者用)", url: "https://kashsan-testsite.top", comment: "Vue Laravel Linux Server iTerm2(SSH接続)", },
-  { id:7, date:"2023", siteName:"猫ジェネ", url: "https://k-ash-dotcom-test.s3.ap-northeast-1.amazonaws.com/index.html", comment: "HTML Javascript AWS S3", },
-  { id:8, date:"2023", siteName:"ピクチャーサーチ", url: "https://kashsan-sample.s3.ap-northeast-1.amazonaws.com/index.html", comment: "React AWS S3", },
-  { id:9, date:"2025", siteName:"当サイト", url: "https://next-blog-app-8uuo.vercel.app", comment: "Nextjs MongoDB", },
+  { id:5, date:"2025", siteName:"K-Ash Mall", url: "https://www.k-ash.com//k-ash_mall", comment: "HTML CSS JavaScript jQuery", },
+  { id:6, date:"2025", siteName:"Fukuyasan", url: "https://www.k-ash.com/real_estate", comment: "HTML CSS JavaScript jQuery", },
+  { id:7, date:"2025", siteName:"SNS(技術者用)", url: "https://kashsan-testsite.top", comment: "Vue Laravel Linux Server iTerm2(SSH接続)", },
+  { id:8, date:"2023", siteName:"猫ジェネ", url: "https://k-ash-dotcom-test.s3.ap-northeast-1.amazonaws.com/index.html", comment: "HTML Javascript AWS S3", },
+  { id:9, date:"2023", siteName:"ピクチャーサーチ", url: "https://kashsan-sample.s3.ap-northeast-1.amazonaws.com/index.html", comment: "React AWS S3", },
+  { id:10, date:"2025", siteName:"当サイト", url: "https://next-blog-app-8uuo.vercel.app", comment: "Nextjs MongoDB", },
 
 ]
