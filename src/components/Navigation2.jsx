f42eb444680f298defe0bd7e45888e07680f3541
Kashsan <kashsan_jp@yahoo.co.jp>
2026-05-30 17:42:20 +0900

    2026.5.30_4

--- src/components/Navigation2.jsx
+++ src/components/Navigation2.jsx
@@ -11,25 +11,25 @@ export default async function Navigation() {
   return (
     <nav className='h-1 px-0'>
         <div className='lg:mr-150 sm:mr-0'>
-            <NavLink label="Home" href="/" />
+            <NavLink label="ホーム" href="/" />
         </div>
             { authUser ? 
                     (
                     <div style={{ touchAction: "manipulation", cursor: "pointer" }}
                         className='flex items-center sm:text-sm lg:text-lg sm:gap-0 lg:gap-3'>
-                        <NavLink label="New Post" href="/posts/create" />
-                        <NavLink label="Dashboard" href="/dashboard" />
+                        <NavLink label="投稿する" href="/posts/create" />
+                        {/* <NavLink label="Dashboard" href="/dashboard" /> */}
                         <form action={logout}>
-                            <button className='nav-link'>Logout</button>
+                            <button className='nav-link'>ログアウト</button>
                         </form>
                     </div>
                     ) : (
                     <div style={{ touchAction: "manipulation", cursor: "pointer" }}
                         className='flex items-center sm:text-xs lg:text-lg sm:gap-0 lg:gap-3 py-0'>
-                        {/* <NavLink label="Register" href="/register" /> */}
-                        <NavLink label="Login" href="/login" />
-                        <NavLink label="Tech Memo" href="/tech" />
-                        {/* <NavLink label="Link" href="/link" /> */}
+                        <NavLink label="登録する" href="/register" />
+                        <NavLink label="管理者用" href="/login" />
+                        {/* <NavLink label="Tech Memo" href="/tech" />
+                        <NavLink label="Link" href="/link" /> */}
                     </div>
                 )}
     </nav>
