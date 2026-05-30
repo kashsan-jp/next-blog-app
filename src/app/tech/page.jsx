b2c3dc4324ff10bfd698ba64521d25c4e97bc5b8
Kashsan <kashsan_jp@yahoo.co.jp>
2026-05-30 19:49:45 +0900

    2026.5.30_9

--- src/app/tech/page.jsx
+++ src/app/tech/page.jsx
@@ -6,17 +6,17 @@ export default function Tech() {
   return (
     <div className='body'>
       <div className='title'>開発ノート</div>
-      <p className='text-center text-green-700 sm:text-sm lg:text-xl'>
+      <p className='text-center text-green-900 sm:text-sm lg:text-xl mb-2'>
       開発意図
       </p>
-      <div className='mb-10 p-2 border border-solid rounded m-auto lg:w-lg sm:w-sm'>
+      <div className='mb-10 p-2 border border-none rounded m-auto lg:w-lg sm:w-sm bg-white'>
         当Webアプリは自分自身のWeb技術の向上過程とエラー解決方法を記録に残したく、
         学習も兼ねて技術選定にはNextjsとMongoDBで開発しました。
       </div>
-      <p className='text-center text-green-700 sm:text-sm lg:text-xl'>
+      <p className='text-center text-green-900 sm:text-sm lg:text-xl mb-2'>
       プロフィール
       </p>
-      <div className='p-2 border border-solid rounded m-auto lg:w-lg sm:w-sm border-green-700'>
+      <div className='p-2 border border-none rounded m-auto lg:w-lg sm:w-sm border-green-700  bg-white'>
         1997年HTMLとMemoアプリでWebコーディングを始め、K-Ash.com を立ち上げる。<br></br>
         2005年にJazz&RockバンドE-Specksを結成しG&Voとして活動を開始する。
         2010年に社会貢献と障害者の社会参加のためにMHPコンサートをスタート、
